e83f413ecd6efe7d12fafe3a4245315a4f8460ca
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2024-07-21 15:54:52 -0300

    Add different color to each branch's modifications

--- src/extension/components/DependencyView.tsx
+++ src/extension/components/DependencyView.tsx
@@ -27,16 +27,79 @@ interface DependencyViewProps {
   pull_number: number;
 }
 
+type modLine = {
+  file: string;
+  leftAdded: number[];
+  leftRemoved: number[];
+  rightAdded: number[];
+  rightRemoved: number[];
+};
+
 export default function DependencyView({ owner, repository, pull_number }: DependencyViewProps) {
   const [dependencies, setDependencies] = useState<dependency[]>([]);
   const [diff, setDiff] = useState<string>("");
+  const [modifiedLines, setModifiedLines] = useState<modLine[]>([]);
 
   useEffect(() => {
     getAnalysisOutput(owner, repository, pull_number).then((response) => {
       setDependencies(response.getDependencies());
       setDiff(response.getDiff());
+      setModifiedLines(response.data.modifiedLines ?? []);
     });
   }, [owner, repository, pull_number]);
+
+  useEffect(() => {
+    const updateDiffColors = () => {
+      const diffContainer = document.getElementById("diff-container");
+      const diffFiles = diffContainer?.querySelectorAll(".d2h-file-wrapper");
+
+      if (diffContainer && diffFiles) {
+        // For each modified line, update the colors of the diff
+        for (let modLine of modifiedLines) {
+          const modLineFile = modLine.file;
+
+          // get the line element
+          const diffContent = Array.from(diffFiles).filter((diffFile) => {
+            const fileName = diffFile.querySelector(".d2h-file-name")?.textContent;
+            return fileName?.endsWith(modLineFile);
+          })[0];
+          if (!diffContent) throw new Error(`Diff not found for file ${modLineFile}`);
+
+          // get the insertions and deletions
+          const insertions = diffContent.querySelectorAll("tr:has(td.d2h-ins)");
+          const deletions = diffContent.querySelectorAll("tr:has(td.d2h-del)");
+
+          // update the colors of the insertions
+          for (let line of insertions) {
+            const lineNumber = line.querySelector(".line-num2")?.textContent;
+            console.log("lineNumber", lineNumber);
+            console.log("modLine.leftAdded", modLine.leftAdded);
+            if (lineNumber && modLine.leftAdded.includes(Number.parseInt(lineNumber))) {
+              // Line was added by left
+              console.log("Line was added by left");
+              line.firstElementChild?.classList.remove("d2h-ins");
+              line.firstElementChild?.classList.add("d2h-ins-left");
+              line.lastElementChild?.classList.remove("d2h-ins");
+              line.lastElementChild?.classList.add("d2h-ins-left");
+            }
+          }
+
+          // update the colors of the deletions
+          for (let line of deletions) {
+            const lineNumber = line.querySelector(".line-num2")?.textContent;
+            if (lineNumber && modLine.leftRemoved.includes(Number.parseInt(lineNumber))) {
+              // Line was removed by left
+              line.firstElementChild?.classList.remove("d2h-del");
+              line.firstElementChild?.classList.add("d2h-del-left");
+              line.lastElementChild?.classList.remove("d2h-del");
+              line.lastElementChild?.classList.add("d2h-del-left");
+            }
+          }
+        }
+      }
+    };
+    updateDiffColors();
+  }, [modifiedLines]);
 
   return (
     <>
