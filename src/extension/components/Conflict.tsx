bb912ff297153c4b026e8a37a4568cc6585d356a
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2024-08-09 16:01:22 -0300

    Refactor extract line get method

--- src/extension/components/Conflict.tsx
+++ src/extension/components/Conflict.tsx
@@ -15,38 +15,40 @@ export default function Conflict({ dependency }: ConflictProps) {
     diffLine.scrollIntoView({ block: "center" });
   };
 
-  const gotoDiff = (file: string, line: number) => {
+  const getDiffLine = (file: string, line: number) => {
     // try to get the line element by id
-    const lineElement = document.getElementById(`${file}:${line}`);
-    if (lineElement) {
-      scrollAndHighlight(lineElement);
-      return;
-    }
+    let lineElement = document.getElementById(`${file}:${line}`);
+    if (lineElement) return lineElement;
 
     // if not found, search for the line in the diff
     const diffContainer = document.getElementById("diff-container");
     const diffFiles = diffContainer?.querySelectorAll(".d2h-file-wrapper");
+    if (!diffContainer || !diffFiles) throw new Error("Diff not found");
 
-    if (diffContainer && diffFiles) {
-      // get the diff element of the file
-      const diffContent = Array.from(diffFiles).filter((diffFile) => {
-        const fileName = diffFile.querySelector(".d2h-file-name")?.textContent;
-        return fileName?.endsWith(file);
-      })[0];
-      if (!diffContent) throw new Error(`Diff not found for file ${file}`);
+    // get the diff element of the file
+    const diffContent = Array.from(diffFiles).filter((diffFile) => {
+      const fileName = diffFile.querySelector(".d2h-file-name")?.textContent;
+      return fileName?.endsWith(file);
+    })[0];
+    if (!diffContent) throw new Error(`Diff not found for file ${file}`);
 
-      // get the line element
-      const allLines = diffContent.querySelectorAll(`tr`);
-      const lineElement = Array.from(allLines).filter((l) => {
-        const lineNumber = l.querySelector(".line-num2")?.textContent;
-        return lineNumber === line.toString();
-      })[0];
-      if (!lineElement) throw new Error(`Line ${line} not found in file ${file}`);
+    // get the line element
+    const allLines = diffContent.querySelectorAll(`tr`);
+    lineElement = Array.from(allLines).filter((l) => {
+      const lineNumber = l.querySelector(".line-num2")?.textContent;
+      return lineNumber === line.toString();
+    })[0];
+    if (!lineElement) throw new Error(`Line ${line} not found in file ${file}`);
 
-      // scroll to the line (and set its id)
-      lineElement.id = `${file}:${line}`;
-      scrollAndHighlight(lineElement);
-    }
+    // set the id and return
+    lineElement.id = `${file}:${line}`;
+    return lineElement;
+  };
+
+  const gotoDiff = (file: string, line: number) => {
+    // get the line element and scroll to it
+    const lineElement = getDiffLine(file, line);
+    scrollAndHighlight(lineElement);
   };
 
   return (
