7cee14b5e2164aa412a99a8ce061032e0fd48d4a
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2024-09-18 22:10:14 -0300

    Refactor extract location logic methods from log ui

--- src/extension/components/Conflict.tsx
+++ src/extension/components/Conflict.tsx
@@ -5,32 +5,55 @@ interface ConflictProps {
   setConflict: (dep: dependency) => void;
 }
 
+type locationStrings = {
+  from: string;
+  to: string;
+};
+
 export default function Conflict({ dependency, setConflict }: ConflictProps) {
+  const getLocationFromStackTrace: (dep: dependency) => locationStrings = (dep: dependency) => {
+    if (
+      !dep.body.interference[0].stackTrace ||
+      !dep.body.interference[dep.body.interference.length - 1].stackTrace
+    )
+      throw new Error("File not found: Invalid stack trace");
+
+    const stackTrace0 = dep.body.interference[0].stackTrace[0];
+    const location0 = stackTrace0.class.replaceAll(".", "/") + ".java";
+
+    const stackTraceN = dep.body.interference[dep.body.interference.length - 1].stackTrace![0];
+    const locationN = stackTraceN.class.replaceAll(".", "/") + ".java";
+
+    return {
+      from: `${location0}:${stackTrace0.line}`,
+      to: `${locationN}:${stackTraceN.line}`
+    };
+  };
+
+  const getLocationStrings: (dep: dependency) => locationStrings = (dep: dependency) => {
+    const location0 = dep.body.interference[0].location;
+    const locationN = dep.body.interference[dep.body.interference.length - 1].location;
+
+    if (location0.file === "UNKNOWN" || locationN.file === "UNKNOWN") {
+      return getLocationFromStackTrace(dep);
+    } else {
+      return {
+        from: `${location0.class}:${location0.line}`,
+        to: `${locationN.class}:${locationN.line}`
+      };
+    }
+  };
+
+  const locationStrings = getLocationStrings(dependency);
+
   return (
     <div className="tw-mb-3 tw-cursor-pointer tw-w-fit" onClick={() => setConflict(dependency)}>
       <span>
         {dependency.label} ({dependency.type})&nbsp;
       </span>
-      {dependency.body.interference[0].location.file !== "UNKNOWN" ? (
-        <p className="tw-text-gray-400">
-          in {dependency.body.interference[0].location.class}:{dependency.body.interference[0].location.line}
-          &nbsp;&rarr;&nbsp;
-          {dependency.body.interference[dependency.body.interference.length - 1].location.class}:
-          {dependency.body.interference[dependency.body.interference.length - 1].location.line}
-        </p>
-      ) : dependency.body.interference[0].stackTrace &&
-        dependency.body.interference[dependency.body.interference.length - 1].stackTrace ? (
-        <p className="tw-text-gray-400">
-          in {dependency.body.interference[0].stackTrace[0].class.replaceAll(".", "/")}.java:
-          {dependency.body.interference[0].stackTrace[0].line}
-          &nbsp;&rarr;&nbsp;
-          {dependency.body.interference[
-            dependency.body.interference.length - 1
-          ].stackTrace![0].class.replaceAll(".", "/")}
-          .java:
-          {dependency.body.interference[dependency.body.interference.length - 1].stackTrace![0].line}
-        </p>
-      ) : null}
+      <p className="tw-text-gray-400">
+        in {locationStrings.from} &rarr; {locationStrings.to}
+      </p>
     </div>
   );
 }
