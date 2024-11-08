8278ac5855208df551fa041d33bcda070e3cda55
ykarocin <ysa@cin.ufpe.br>
2024-11-08 19:52:28 -0300

    Adicionando possibilidades de oa

--- src/extension/utils/graph.ts
+++ src/extension/utils/graph.ts
@@ -24,13 +24,7 @@ const generateGraphData = (conflictType: string, data: OAlineData | { [key: stri
     const LC = data["LC"];
     const RC = data["RC"];
 
-    if (`${L.file.split("/").pop()}:${L.line}` === `${LC.file.split("/").pop()}:${LC.line}`){
-      if (`${R.file.split("/").pop()}:${R.line}` === `${RC.file.split("/").pop()}:${RC.line}`){
-        return generateOAGraphDataTwoNodes(L, R);
-      }
-    }else{
-      return generateOAGraphDataFourNodes(L, R, LC, RC);
-    }
+    return generateOAGraphData(L, R, LC, RC);
   } else {
     throw new Error("Conflict type not supported");
   }
@@ -51,7 +45,21 @@ const generateGraphData = (conflictType: string, data: OAlineData | { [key: stri
  * @param RC - line that derives from the line modified by the right side and directly involved in the conflict
  * @returns an object with the nodes and edges for the graph that represents the OA conflict format
  */
-const generateOAGraphDataFourNodes = (L: lineData, R: lineData, LC: lineData, RC: lineData) => {
+const generateOAGraphData = (L: lineData, R: lineData, LC: lineData, RC: lineData) => {
+  if (`${L.file.split("/").pop()}:${L.line}` === `${LC.file.split("/").pop()}:${LC.line}`) {
+      if (`${R.file.split("/").pop()}:${R.line}` === `${RC.file.split("/").pop()}:${RC.line}`){
+        return oaGraphDataTwoNodes(L, R);
+      } else {
+        return oaGraphDataThreeNodesRC(L, R, RC);
+      }
+  } else if (`${R.file.split("/").pop()}:${R.line}` === `${RC.file.split("/").pop()}:${RC.line}`) {
+    return oaGraphDataThreeNodesLC(L, R, LC);
+  } else {
+    return oaGraphDataFourNodes(L, R, LC, RC);
+  }
+}
+
+const oaGraphDataFourNodes = (L: lineData, R: lineData, LC: lineData, RC: lineData) => {
   const nodes = [
     {
       key: "0",
@@ -145,7 +153,7 @@ const generateOAGraphDataFourNodes = (L: lineData, R: lineData, LC: lineData, RC
   return { nodes, edges };
 };
 
-const generateOAGraphDataTwoNodes = (L: lineData, R: lineData) => {
+const oaGraphDataTwoNodes = (L: lineData, R: lineData) => {
   const nodes = [
     {
       key: "0",
@@ -187,5 +195,150 @@ const generateOAGraphDataTwoNodes = (L: lineData, R: lineData) => {
   return { nodes, edges };
 };
 
+const oaGraphDataThreeNodesRC = (L: lineData, R: lineData, RC: lineData) => {
+  const nodes = [
+    {
+      key: "0",
+      attributes: {
+        x: 0,
+        y: 0,
+        label: `${L.file.split("/").pop()}:${L.line}`,
+        size: NODE_SIZE,
+        color: NODE_COLOR,
+        labelPosition: "top"
+      }
+    },
+    {
+      key: "1",
+      attributes: {
+        x: 1,
+        y: 0,
+        label: `${R.file.split("/").pop()}:${R.line}`,
+        size: NODE_SIZE,
+        color: NODE_COLOR,
+        labelPosition: "right"
+      }
+    },
+    {
+      key: "2",
+      attributes: {
+        x: 1,
+        y: -1,
+        label: `${RC.file.split("/").pop()}:${RC.line}`,
+        size: NODE_SIZE,
+        color: NODE_COLOR,
+        labelPosition: "right"
+      }
+    }
+  ];
+
+  const edges = [
+    {
+      source: "0",
+      target: "1",
+      attributes: {
+        color: EDGE_COLOR_PRECEDES,
+        size: EDGE_SIZE,
+        type: "arrow",
+        label: "Precedes"
+      }
+    },
+    {
+      source: "0",
+      target: "2",
+      attributes: {
+        color: EDGE_COLOR_OA,
+        size: EDGE_SIZE,
+        type: "arrow",
+        label: "OA"
+      }
+    },
+    {
+      source: "1",
+      target: "2",
+      attributes: {
+        color: EDGE_COLOR_CALL,
+        size: EDGE_SIZE,
+        type: "arrow",
+        label: "Call"
+      }
+    },
+  ];
+
+  return { nodes, edges };
+};
+
+const oaGraphDataThreeNodesLC = (L: lineData, R: lineData, LC: lineData) => {
+  const nodes = [
+    {
+      key: "0",
+      attributes: {
+        x: 0,
+        y: 0,
+        label: `${L.file.split("/").pop()}:${L.line}`,
+        size: NODE_SIZE,
+        color: NODE_COLOR,
+        labelPosition: "top"
+      }
+    },
+    {
+      key: "1",
+      attributes: {
+        x: 1,
+        y: 0,
+        label: `${R.file.split("/").pop()}:${R.line}`,
+        size: NODE_SIZE,
+        color: NODE_COLOR,
+        labelPosition: "right"
+      }
+    },
+    {
+      key: "2",
+      attributes: {
+        x: 0,
+        y: -1,
+        label: `${LC.file.split("/").pop()}:${LC.line}`,
+        size: NODE_SIZE,
+        color: NODE_COLOR,
+        labelPosition: "bottom"
+      }
+    }
+  ];
+
+  const edges = [
+    {
+      source: "0",
+      target: "1",
+      attributes: {
+        color: EDGE_COLOR_PRECEDES,
+        size: EDGE_SIZE,
+        type: "arrow",
+        label: "Precedes"
+      }
+    },
+    {
+      source: "0",
+      target: "2",
+      attributes: {
+        color: EDGE_COLOR_CALL,
+        size: EDGE_SIZE,
+        type: "arrow",
+        label: "Call"
+      }
+    },
+    {
+      source: "2",
+      target: "1",
+      attributes: {
+        color: EDGE_COLOR_OA,
+        size: EDGE_SIZE,
+        type: "arrow",
+        label: "OA"
+      }
+    },
+  ];
+
+  return { nodes, edges };
+};
 
 export { generateGraphData, type lineData };
