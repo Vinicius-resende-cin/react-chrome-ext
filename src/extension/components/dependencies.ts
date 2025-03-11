fde147d99b213f7f3cd3a171578670199cbce379
Vinicius Resende <92769975+Vinicius-resende-cin@users.noreply.github.com>
2025-03-11 12:34:58 -0300

    Fix cf conflicts

--- src/extension/components/dependencies.ts
+++ src/extension/components/dependencies.ts
@@ -22,7 +22,7 @@ const filterDuplicatedDependencies = (dependencies: dependency[]) => {
   return uniqueDependencies;
 };
 
-const getLastValidNode = (stackTrace: tracedNode[]) => {
+const getLastValidNode = (stackTrace: tracedNode[], maxDepth: number) => {
   // get all the diff file elements
   let diffFiles: NodeListOf<Element> | Element[] | undefined = document
     .getElementById("diff-container")
@@ -30,7 +30,7 @@ const getLastValidNode = (stackTrace: tracedNode[]) => {
   if (!diffFiles) throw new Error("Diff not found");
   diffFiles = Array.from(diffFiles);
 
-  let cur = stackTrace.length - 1;
+  let cur = maxDepth;
   while (cur >= 0) {
     const file = stackTrace[cur].class.replaceAll(".", "/") + ".java";
 
@@ -51,7 +51,12 @@ const getLastValidNode = (stackTrace: tracedNode[]) => {
 };
 
 const updateLocationFromStackTrace = (dep: dependency, options?: { inplace?: boolean; mode?: "default" | "deep" }) => {
-  if (!dep.body.interference[0].stackTrace || !dep.body.interference[dep.body.interference.length - 1].stackTrace)
+  console.log("Updating location from stack trace");
+  if (
+    !dep.body.interference[0].stackTrace ||
+    (dep.type.startsWith("CONFLUENCE") && !dep.body.interference[1].stackTrace) ||
+    (!dep.type.startsWith("CONFLUENCE") && !dep.body.interference[dep.body.interference.length - 1].stackTrace)
+  )
     throw new Error("File not found: Invalid stack trace");
 
   const inplace: boolean = options?.inplace || false;
@@ -59,10 +64,31 @@ const updateLocationFromStackTrace = (dep: dependency, options?: { inplace?: boo
 
   let stackTrace0: tracedNode;
   let stackTraceN: tracedNode;
+  let stackTraceCF: tracedNode | null = null;
 
   if (mode === "deep") {
-    stackTrace0 = getLastValidNode(dep.body.interference[0].stackTrace);
-    stackTraceN = getLastValidNode(dep.body.interference[dep.body.interference.length - 1].stackTrace!);
+    const maxDepth0 = dep.type.startsWith("CONFLUENCE")
+      ? dep.body.interference[0].stackTrace.length - 2
+      : dep.body.interference[0].stackTrace.length - 1;
+    const maxDepthN = dep.type.startsWith("CONFLUENCE")
+      ? dep.body.interference[1].stackTrace!.length - 2
+      : dep.body.interference[dep.body.interference.length - 1].stackTrace!.length - 1;
+
+    stackTrace0 = getLastValidNode(dep.body.interference[0].stackTrace, maxDepth0);
+    stackTraceN = getLastValidNode(
+      dep.type.startsWith("CONFLUENCE")
+        ? dep.body.interference[1].stackTrace!
+        : dep.body.interference[dep.body.interference.length - 1].stackTrace!,
+      maxDepthN
+    );
+  } else if (dep.type.startsWith("CONFLUENCE")) {
+    stackTrace0 = dep.body.interference[0].stackTrace[0];
+    stackTraceN = dep.body.interference[1].stackTrace![0];
+    stackTraceCF = {
+      class: dep.body.interference[dep.body.interference.length - 1].location.class,
+      method: dep.body.interference[dep.body.interference.length - 1].location.method,
+      line: dep.body.interference[dep.body.interference.length - 1].location.line
+    };
   } else {
     stackTrace0 = dep.body.interference[0].stackTrace[0];
     stackTraceN = dep.body.interference[dep.body.interference.length - 1].stackTrace![0];
@@ -70,25 +96,48 @@ const updateLocationFromStackTrace = (dep: dependency, options?: { inplace?: boo
 
   const file0 = stackTrace0.class.replaceAll(".", "/") + ".java";
   const fileN = stackTraceN.class.replaceAll(".", "/") + ".java";
+  const fileCF = stackTraceCF ? stackTraceCF.class.replaceAll(".", "/") + ".java" : "";
 
   if (inplace) {
-    dep.body.interference[0].location.file = file0;
-    dep.body.interference[0].location.line = stackTrace0.line;
-    dep.body.interference[0].location.class = stackTrace0.class;
-    dep.body.interference[dep.body.interference.length - 1].location.file = fileN;
-    dep.body.interference[dep.body.interference.length - 1].location.line = stackTraceN.line;
-    dep.body.interference[dep.body.interference.length - 1].location.class = stackTraceN.class;
+    let firstNode = dep.body.interference[0];
+    let lastNode = dep.type.startsWith("CONFLUENCE")
+      ? dep.body.interference[1]
+      : dep.body.interference[dep.body.interference.length - 1];
+    let cfNode = dep.type.startsWith("CONFLUENCE") ? dep.body.interference[dep.body.interference.length - 1] : null;
+
+    firstNode.location.file = file0;
+    firstNode.location.line = stackTrace0.line;
+    firstNode.location.class = stackTrace0.class;
+    lastNode.location.file = fileN;
+    lastNode.location.line = stackTraceN.line;
+    lastNode.location.class = stackTraceN.class;
+    if (cfNode && stackTraceCF) {
+      cfNode.location.file = fileCF;
+      cfNode.location.line = stackTraceCF.line;
+      cfNode.location.class = stackTraceCF.class;
+    }
 
     return dep;
   } else {
     const newDep = structuredClone(dep);
 
-    newDep.body.interference[0].location.file = file0;
-    newDep.body.interference[0].location.line = stackTrace0.line;
-    newDep.body.interference[0].location.class = stackTrace0.class;
-    newDep.body.interference[dep.body.interference.length - 1].location.file = fileN;
-    newDep.body.interference[dep.body.interference.length - 1].location.line = stackTraceN.line;
-    newDep.body.interference[dep.body.interference.length - 1].location.class = stackTraceN.class;
+    let firstNode = newDep.body.interference[0];
+    let lastNode = newDep.type.startsWith("CONFLUENCE")
+      ? newDep.body.interference[1]
+      : newDep.body.interference[dep.body.interference.length - 1];
+    let cfNode = newDep.type.startsWith("CONFLUENCE") ? newDep.body.interference[dep.body.interference.length - 1] : null;
+
+    firstNode.location.file = file0;
+    firstNode.location.line = stackTrace0.line;
+    firstNode.location.class = stackTrace0.class;
+    lastNode.location.file = fileN;
+    lastNode.location.line = stackTraceN.line;
+    lastNode.location.class = stackTraceN.class;
+    if (cfNode && stackTraceCF) {
+      cfNode.location.file = fileCF;
+      cfNode.location.line = stackTraceCF.line;
+      cfNode.location.class = stackTraceCF.class;
+    }
 
     return newDep;
   }
